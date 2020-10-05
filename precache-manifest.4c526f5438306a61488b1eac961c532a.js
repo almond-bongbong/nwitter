47af605dfc868e93611adf6e855f2c8f82884a68
cmlee <bal.dongdongdong@gmail.com>
2020-10-05 11:25:34 +0900

    Updates

---
+++ precache-manifest.4c526f5438306a61488b1eac961c532a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b4b3625bf02ad3214a0dc5c7c7632a8",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "62609e25df956c32bbda",
+    "url": "/nwitter/static/css/main.51fba151.chunk.css"
+  },
+  {
+    "revision": "120cb061dd5e58990b44",
+    "url": "/nwitter/static/js/2.74a9ef3c.chunk.js"
+  },
+  {
+    "revision": "2c0b89fe32f6d0b561deea7159cbd810",
+    "url": "/nwitter/static/js/2.74a9ef3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62609e25df956c32bbda",
+    "url": "/nwitter/static/js/main.d296805a.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
